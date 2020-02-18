6640ffe25a043c6a906ff3b0649e3da28df63d41
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-02-18 19:53:49 +0530

    routes added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component'
+import { SignupComponent } from './signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:LoginComponent },
+  { path:'/login', component:LoginComponent },
+  { path:'/signup', component:SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
