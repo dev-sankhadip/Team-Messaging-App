248dc84c1f86137a8fb3fc788ebb6d0ce843e573
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-02-21 13:50:04 +0530

    feat: group discussion added using websocket

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { FormGroup, FormControl } from '@angular/forms'
+
+
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route:ActivatedRoute ) { }
+
+  public id:String
+  public chatSocket
+  public socketURL='127.0.0.1:8000'
+
+  chatForm=new FormGroup({
+    message:new FormControl('')
+  })
+
 
   ngOnInit(): void {
+    this.id=this.route.snapshot.params['id'];
+    console.log(this.id)
+    this.chatSocket = new WebSocket('ws://' + this.socketURL + '/ws/chat/' + this.id + '/');
+    let socketCon=this.chatSocket;
+    this.chatSocket.onmessage=function(e)
+    {
+      console.log(e);
+      var data = JSON.parse(e.data);
+      var message = data['message'];
+      document.querySelector('#chat-log').value += (message + '\n');
+    }
+    this.chatSocket.onopen=function(e)
+    {
+      console.log(e);
+    }
+    this.chatSocket.onclose = function(e) {
+      console.error('Chat socket closed unexpectedly');
+    };
+
+  //   document.querySelector('#chat-message-submit').onclick = function(e) {
+  //     var messageInputDom = document.querySelector('#chat-message-input');
+  //     var message = messageInputDom.value;
+  //     chatSocket.send(JSON.stringify({
+  //         'message': message
+  //     }));
+
+  //     messageInputDom.value = '';
+  // };
+    // document.getElementById("chat-message-submit").onclick=function(e)
+    // {
+    //   const messageInputDom=document.getElementById("chat-message-input");
+    //   const message=messageInputDom.value;
+    //   socketCon.send(JSON.stringify({
+    //     'message':message
+    //   }))
+    //   messageInputDom.value='';
+    // }
+
+
   }
 
+  send()
+  {
+    console.log(this.chatForm.value);
+    this.chatSocket.send(JSON.stringify({
+      'message':this.chatForm.value.message
+    }))
+    this.chatForm.reset()
+  }
 }
